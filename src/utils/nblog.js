33489f15926bdd7ac861bc1078e79a2e4a3df5df
Suleiman Ibrahim <64266194+princeibs@users.noreply.github.com>
2022-08-31 20:37:44 +0100

    Update nblog.js

--- src/utils/nblog.js
+++ src/utils/nblog.js
@@ -1,24 +1,31 @@
 import { parseNearAmount } from "near-api-js/lib/utils/format";
 
+// amount of gas to send along with transaction
+// Unused gas is returned to sender
 const GAS = 100000000000000;
 
+// This function creates a new blog
 export function createBlog(blog) {
   return window.contract.createBlog({ blogData: blog });
 }
 
+// This function likes a blog with the give slug
 export function likeBlog(slug) {
   return window.contract.likeBlog({ slug });
 }
 
+// This function tips an author a specific amount (in NEAR coins) for their blog
 export async function tipAuthor(slug, amount) {
   const fAmount = parseNearAmount(amount + "");
   await window.contract.buyCoffee({ slug }, GAS, fAmount);
 }
 
+// This function returns a specific blog with the given slug
 export async function viewBlog(slug) {
   return await window.contract.viewBlog({ slug });
 }
 
+// This function returns all blogs stored in the blockchain
 export function allBlogs() {
   return window.contract.allBlogs();
 }
